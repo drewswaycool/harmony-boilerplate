0c5dc66abcb31b7521230215c0b64c27a837df6a
refaelok <refaeloknin@gmail.com>
2017-04-30 22:39:27 +0300

    redux-websocket-action

--- client/src/utils/redux-websocket-action.js
+++ client/src/utils/redux-websocket-action.js
@@ -1,26 +1,25 @@
-import wsReconnect from 'ws-reconnect';
+//import wsReconnect from 'ws-reconnect';
 
 class WSActions {
 
     constructor(store, url, options) {
         this.store = store;
         this.url = url;
-        this.socket = new wsReconnect(url, options);
+        this.socket = new WebSocket(url);
     }
 
     start() {
         console.log(this.socket);
-        this.socket.start();
-        this.socket.on("message",this.onMessage.bind(this));
-    }
-
-    stop() {
-        this.socket.destroy();
+        this.socket.onmessage = this.onMessage.bind(this);
     }
 
     onMessage(message) {
         console.log(message);
         console.log(this.store);
+        this.store.dispatch({
+            type: 'FETCH_POSTS',
+            payload: null
+        });
     }
 
 }
