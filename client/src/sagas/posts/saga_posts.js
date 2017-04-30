ffb3d18ca8c11227ff039ec5ed9873f72b307bc6
refaelok <refaeloknin@gmail.com>
2017-05-01 00:34:26 +0300

    redux-websocket-action

--- client/src/sagas/posts/saga_posts.js
+++ client/src/sagas/posts/saga_posts.js
@@ -5,8 +5,7 @@ import { PORTAL } from '../../routes';
 export function* fetchPosts(api) {
 
     try {
-        // const response = yield call(api.fetchPosts);
-        const response = yield call(api.fetchPostsWithWS);
+        const response = yield call(api.fetchPosts);
 
         if (response.data && response.data.length > 0) {
             yield put({type: ActionTypes.FETCH_POSTS_SUCCESS, posts: response.data});
@@ -35,7 +34,9 @@ export function* fetchPost(api, action) {
 export function* createPost(api, action) {
 
     try {
-        const response = yield call(api.createPost, action.payload);
+        // const response = yield call(api.createPost, action.payload);
+        const response = yield call(api.createPostWithWS, action.payload);
+
         if (response.data.message === "Resource created") {
             yield put({type: ActionTypes.NAVIGATE_TO, path: PORTAL});
         }
