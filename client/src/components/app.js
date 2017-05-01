54bf16a405fec52a508922b988cdff2ced85020a
refaelok <refaeloknin@gmail.com>
2017-05-02 01:56:02 +0300

    websocket actions docomentation

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Component } from 'react';
-import Navigation from '../containers/navigation/Navigation'
+import Navigation from '../containers/navigation/Navigation';
+import Localization from '../containers/localization/Localization';
+
 
 export default class App extends Component {
     render() {
         return (
             <div>
+                <Localization/>
                 {this.props.children}
                 <Navigation/>
             </div>
